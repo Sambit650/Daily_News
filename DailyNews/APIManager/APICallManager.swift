dcc0c7078d433664be68285d954667379bd00ff1
Sambit <sambit650@gmail.com>
2021-02-06 10:40:58 +0530

    Added code indentation in APICallManger

--- DailyNews/APIManager/APICallManager.swift
+++ DailyNews/APIManager/APICallManager.swift
@@ -23,54 +23,54 @@ enum Encoding {
 }
 
 class APICallManager {
-
+    
     static let instance = APICallManager()
-
+    
     //MARK: - MBProgressHUD Loader Methods
-       //Start Loader
-       static func showGlobalProgressHUDWithTitle(_ title: String) {
-           let window = UIApplication.shared.windows[0]
-           MBProgressHUD.hide(for: window, animated: true)
-           let hud = MBProgressHUD.showAdded(to: window, animated: true)
-           hud.mode = .indeterminate
-           hud.backgroundView.isUserInteractionEnabled = true
-           hud.isUserInteractionEnabled = true
-           hud.label.text = title
-       }
-
-       //Stop Loader
-       static func dismissGlobalHUD()->(Void) {
-           DispatchQueue.main.async {
-               let window = UIApplication.shared.windows[0]
-               MBProgressHUD.hide(for: window, animated: true)
-           }
-       }
-
+    //Start Loader
+    static func showGlobalProgressHUDWithTitle(_ title: String) {
+        let window = UIApplication.shared.windows[0]
+        MBProgressHUD.hide(for: window, animated: true)
+        let hud = MBProgressHUD.showAdded(to: window, animated: true)
+        hud.mode = .indeterminate
+        hud.backgroundView.isUserInteractionEnabled = true
+        hud.isUserInteractionEnabled = true
+        hud.label.text = title
+    }
+    
+    //Stop Loader
+    static func dismissGlobalHUD()->(Void) {
+        DispatchQueue.main.async {
+            let window = UIApplication.shared.windows[0]
+            MBProgressHUD.hide(for: window, animated: true)
+        }
+    }
+    
     // Create request
-       class func createRequest(
-         _ url: String,
-         method: HTTPMethod,
-         headers: [String: String]?,
-         parameters: AnyObject?,
-         onSuccess successCallback: ((JSON) -> Void)?,
-         onFailure failureCallback: ((String) -> Void)?
-         ) {
-         
-         //Call API with ALamofire
-         Alamofire.request(url, method: method, parameters: parameters as? Parameters, encoding: JSONEncoding.default, headers:  headers).validate().responseJSON { response in
-                 switch response.result {
-                 case .success(let value):
-                     let json = JSON(value)
-                    // print("json :\(json)")
-                     successCallback?(json)
-                 case .failure(let error):
-                     if let callback = failureCallback {
-                     // Return
-                     callback(error.localizedDescription)
-                 }
-             }
-         }
-     }
-
+    class func createRequest(
+        _ url: String,
+        method: HTTPMethod,
+        headers: [String: String]?,
+        parameters: AnyObject?,
+        onSuccess successCallback: ((JSON) -> Void)?,
+        onFailure failureCallback: ((String) -> Void)?
+    ) {
+        
+        //Call API with ALamofire
+        Alamofire.request(url, method: method, parameters: parameters as? Parameters, encoding: JSONEncoding.default, headers:  headers).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                // print("json :\(json)")
+                successCallback?(json)
+            case .failure(let error):
+                if let callback = failureCallback {
+                    // Return
+                    callback(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
 }
 
